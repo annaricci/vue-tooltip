df4f345dd150082352479a056e044a2caee32fd0
Anna Ricci Benson <angels.ricci5@gmail.com>
2020-12-29 19:27:34 +0530

    prod chage

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:annaricci/vue-tooltip.git master:gh-pages
+git push -f git@github.com:annaricci/vue-tooltip.git master:main
 cd -
