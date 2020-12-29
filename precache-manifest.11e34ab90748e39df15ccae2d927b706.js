d4cfd294097d934e0c667e82a069b774cc7dd6ee
Anna Ricci Benson <angels.ricci5@gmail.com>
2020-12-29 20:13:42 +0530

    deploy

---
+++ precache-manifest.11e34ab90748e39df15ccae2d927b706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73c10910e85df53f3dd8",
+    "url": "/tooltip/css/app.994ca273.css"
+  },
+  {
+    "revision": "7fd98f40f833be6a5999",
+    "url": "/tooltip/css/chunk-vendors.dcd8433d.css"
+  },
+  {
+    "revision": "277381ce8c061b2f7781b551a8e02309",
+    "url": "/tooltip/index.html"
+  },
+  {
+    "revision": "73c10910e85df53f3dd8",
+    "url": "/tooltip/js/app.9143cd0a.js"
+  },
+  {
+    "revision": "7fd98f40f833be6a5999",
+    "url": "/tooltip/js/chunk-vendors.9559404c.js"
+  },
+  {
+    "revision": "462c8f48ad671a8b8a80690523d2b4d3",
+    "url": "/tooltip/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tooltip/robots.txt"
+  }
+]);
